b873786d464541e7b479c8bde482adff4a73cf40
Pat Nakajima <patnakajima@gmail.com>
2009-06-16 03:13:57 -0400

    Unfinished business.

--- lib/rip/commands/server.rb
+++ lib/rip/commands/server.rb
@@ -2,7 +2,7 @@ module Rip
   module Commands
     def server(options = {}, package = nil, *args)
       $PACKAGES = manager.packages
-      require File.join(File.dirname(__FILE__), 'runner')
+      require File.join(File.dirname(__FILE__), 'app')
       RipServer.run!
     end
   end
